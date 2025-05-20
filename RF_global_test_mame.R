8a0fcf7151edfc6c84898d9c57841ac262cf8404
Mame Cheikh GADIAGA <gadiaga@rennes-angers.loc>
2025-05-20 09:26:07 +0200

    Random Forest final

--- RF_global_test_mame.R
+++ RF_global_test_mame.R
@@ -7,7 +7,8 @@ library(raster)
 library(writexl)
 library(foreach)
 library(tidyverse)
-
+library(tmap)
+library(viridisLite) 
 
 #Définition des fonctions ----
 Myeval <- function(x, y){
@@ -41,19 +42,26 @@ Myeval <- function(x, y){
   return(evalRes)
 }
 
-
-# Définition des paramètres à tester-----
-liste_ntree <- seq(50,1000,50)
-
-# Chargement des données----
-# changer le répertoire de travail
+#changer le répertoire de travail
 setwd("Y:/BDAT/traitement_donnees/MameGadiaga/resultats")
 
+# Définition des paramètres à tester-----
+liste_ntree <- seq(50,500,50)
+
+# Chargement des données----
+
+dpt <- st_read("Y:/BDAT/traitement_donnees/MameGadiaga/prétraitement/Analyse/Codes_Mame/Donnees/dept_53.shp")
+gXY <- readRDS("gXY.rds")
+rast_za <- rast("Y:/BDAT/traitement_donnees/MameGadiaga/resultats/rast_za.tif")
 data<- readRDS("matrice_covariables.rds")
 
 datacov_s <- data %>%
   select(-c(id_profil, bdatid, insee, codification)) %>%
   na.omit()
+
+# datacov_s <- data %>%
+#   select(-c(X,Y,INSEE_COM)) %>%
+#   na.omit()
 
 # définir les colonnes de covariables
 covs <- datacov_s[, 7:70]  
@@ -66,7 +74,7 @@ idvar <- "pH"
 Y <- datacov_s[[idvar]]
 X <- datacov_s[, idcovs]
 
-# Sélection d'un sous ensemble de variables -------------
+# Sélection d'un sous ensemble de variables -----
 #Boruta sélectionne les variables pertinentes en se basant sur l’importance de permutation
 # result_brt <- Boruta(X, Y)
 # 
@@ -84,12 +92,12 @@ load("liste_variables_boruta.rds")
 load("cov_brt_C.rds")
 
 # créer un sous-jeu de données avec les variables retenues par Boruta
+# X2 <-  datacov_s[, c("id", "moy_ph", cov_brt)]
 X2 <-  datacov_s[, c("id", "pH", cov_brt)]
 
+# Calibrage des hyperparamètres -----
 
-# Calibrage des hyperparamètres -----------------
-
-## mettre en place la parallélisation (sera utilisée pour la kCV) ----------
+## mettre en place la parallélisation (sera utilisée pour la kCV) 
 #nombre de coeurs utilisés
 n.cores <- parallel::detectCores() - 1
 
@@ -105,7 +113,7 @@ doParallel::registerDoParallel(cl = my.cluster)
 taille  <- ncol(X2) - 2                    # nb de variables explicatives
 folds   <- pls::cvsegments(N = length(Y), k = 10)
 
-#Calibrage du modèle
+##Calibrage du modèle
 res_calib <- foreach(
   nt        = liste_ntree,               # valeur de ntree (une par tâche)
   .combine  = rbind,
@@ -113,7 +121,7 @@ res_calib <- foreach(
   .export   = c("X2", "Y", "taille", "folds", "Myeval")
 ) %dopar% {
 
-  ## -- Étape 1 : Tune mtry -----
+  ##Étape 1 : Tune mtry 
   bestmtry <- tuneRF(
     x          = X2[, -c(1, 2)],
     y          = Y,
@@ -126,13 +134,13 @@ res_calib <- foreach(
   )
   mtry_opt <- bestmtry[which.min(bestmtry[, "OOBError"]), "mtry"]
 
-  ## -- Étape 2 : 10-fold CV ----------
+  ## Étape 2 : 10-fold CV 
   preds <- numeric(length(Y))              # vecteur final de prédictions
 
   for (seg in folds) {
     mod <- ranger(
-      formula      = pH ~ .,
-      data         = X2[-seg, -1],         # -1 supprime la colonne id
+      formula      = C ~ .,
+      data         = X2[-seg, -1],         
       num.trees    = nt,
       mtry         = mtry_opt,
       min.node.size = 3,
@@ -145,20 +153,20 @@ res_calib <- foreach(
 
     preds[seg] <- predict(
       mod,
-      data       = X2[seg, -c(1, 2)],      # set de test (sans C et id)
+      data       = X2[seg, -c(1, 2)],      
       type       = "quantiles",
       quantiles  = 0.5
     )$predictions
   }
 
-  ## -- Étape 3 : métriques
+  ## Étape 3 : métriques
   metrique         <- Myeval(preds, Y)
   metrique$ntree   <- nt
   metrique$mtry    <- mtry_opt
-  metrique                              # renvoyé à .combine = rbind
+  metrique                              
 }
 
-saveRDS(res_calib, file = "resultats_calib_pH.rds")
+saveRDS(res_calib, file = "resultats_calib_c.rds")
 
 # res_calib_list <- lapply(liste_ntree, function(nt) {
 #   
@@ -216,44 +224,29 @@ saveRDS(res_calib, file = "resultats_calib_pH.rds")
 # 
 # res_calib<-bind_rows(res_calib_list)
 
-# 3. Libération des ressources
+# Libération des ressources
 parallel::stopCluster(my.cluster)
 
-saveRDS(res_calib, file = "resultats_calib_C.rds")
+resC<-readRDS("resultats_calib_C.rds")
+respH<-readRDS("resultats_calib_pH.rds")
+respH_cent<-readRDS("result_calib_pH_centroides.rds")
 
+# saveRDS(res_calib, file = "resultats_calib_C.rds")
 
-resultats <- readRDS("resultats_calib_C.rds")
+ ### Le modèle retenu est celui avec ntree=500 et mtry_opt=14
 
+# Visualisation de l'importance des variables----
 
-
-
-# res1<- readRDS("resultats_calib.Rds")
-# res2<- readRDS("resultats_calib2.Rds")
-# 
-# res_calib <- rbind(res1, res2)
-
-# # Graphe des métriques selon ntree
-# 
-# res_calib <- res_calib %>%
-#   pivot_longer(cols = c(ME, MAE, RMSE, r2, NSE), names_to = "metrique", values_to = "valeur")
-# 
-# ggplot(res_calib, aes(x = factor(ntree), y = valeur, fill = metrique)) +
-#   geom_bar(stat = "identity", position = "dodge") +
-#   labs(x = "Nombre d'arbres (ntree)", y = "Valeur de la métrique", title = "Performance du modèle RF selon ntree") +
-#   theme_minimal()
-
- 
-  # Étape 4 : Visualisation de l'importance des
-
+##Pour le pH
 
   QRF_Mod.G <- ranger(
-    formula = C ~ .,
+    formula = pH ~ .,
     data = X2[, !(names(X2) %in% c("id"))],
-    num.trees = ,
+    num.trees = 350,
     min.node.size = 3,
     quantreg = TRUE,
     max.depth = 15,
-    mtry = mtry_opt,
+    mtry = 11,
     importance = "permutation",
     scale.permutation.importance = TRUE
   )
@@ -262,7 +255,7 @@ resultats <- readRDS("resultats_calib_C.rds")
                         Variable = names(QRF_Mod.G$variable.importance)) %>%
     arrange(desc(Importance))
 
-  plot_path <- paste0("Y:/BDAT/traitement_donnees/MameGadiaga/resultats/varimp_ntree_", ntree_val, ".png")
+  plot_path <- paste0("Y:/BDAT/traitement_donnees/MameGadiaga/resultats/variables_importantes.png")
   png(plot_path, width = 800, height = 600)
   print(
     ggplot(Imp_QRF, aes(x = reorder(Variable, Importance), y = Importance)) +
@@ -271,7 +264,126 @@ resultats <- readRDS("resultats_calib_C.rds")
       coord_flip() +
       ylab("Importance (Permutation)") +
       xlab("Covariables") +
-      ggtitle(paste("Importance des covariables - ntree =", ntree_val))
+      ggtitle(paste("Importance des covariables"))
   )
   dev.off()
 
+#Application du modèle----
+  
+  #Sélection des covariables spatiales (mêmes que pour le modèle)
+  testD <- gXY %>% 
+    select(all_of(cov_brt))
+  
+  # Prédiction des quantiles (5%, médiane, 95%)
+  QRF_Median <- predict(
+    QRF_Mod.G,               
+    data = testD,            
+    type = "quantiles",
+    quantiles = c(0.05, 0.5, 0.95),  
+    num.threads = parallel::detectCores() - 1  
+  )$prediction
+
+  #Fusion avec les coordonnées spatiales
+
+  QRF_Median50 <- bind_cols(
+    gXY %>% 
+      filter_at(vars(all_of(cov_brt)), all_vars(!is.na(.))) %>% 
+      select(x, y),      
+    data.frame(                 
+      Q5 = QRF_Median[, 1],     
+      Median = QRF_Median[, 2], 
+      Q95 = QRF_Median[, 3]     
+    )
+  )
+  
+  # Création d'un raster pour chaque quantile
+  rast_q5 <- rast(QRF_Median50 %>% select(x, y, Q5), type = "xyz", crs= "EPSG:2154")
+  rast_median <- rast(QRF_Median50 %>% select(x, y, Median), type = "xyz",crs= "EPSG:2154")
+  rast_q95 <- rast(QRF_Median50 %>% select(x, y, Q95), type = "xyz", crs= "EPSG:2154")
+  crs(rast_za) <- "EPSG:2154"
+  
+  rast_q5 <- extend(rast_q5, rast_za, snap = "near")
+  rast_median <- extend(rast_median, rast_za, snap = "near")
+  rast_q95 <- extend(rast_q95, rast_za, snap = "near")
+  
+  #Application du masque aux rasters
+  rast_q5 <- mask(rast_q5, rast_za)
+  rast_median <- mask(rast_median, rast_za)
+  rast_q95 <- mask(rast_q95, rast_za)
+  
+  # Sauvegarde des rasters
+  writeRaster(rast_q5, "Y:/BDAT/traitement_donnees/MameGadiaga/resultats/rast_q5.tif", filetype="GTiff",overwrite=TRUE)
+  writeRaster(rast_median, "Y:/BDAT/traitement_donnees/MameGadiaga/resultats/rast_median.tif", filetype="GTiff",overwrite=TRUE)
+  writeRaster(rast_q95, "Y:/BDAT/traitement_donnees/MameGadiaga/resultats/rast_q95.tif", filetype="GTiff",overwrite=TRUE)
+  
+#charger raster 
+rast_median_cent <- rast("Y:/BDAT/traitement_donnees/MameGadiaga/resultats/rast_median_centr.tif")
+#représentation graphique----
+  
+  brk   <- c(-Inf, 6, 6.3, 6.5, 6.8, 6.7,  Inf)          
+  labs  <- c("[4.3 - 6[",
+             "[6 – 6.3[",
+             "[6.3 – 6.5[",
+             "[6.5 – 6.8[",
+             "[6.8 – 7[",
+             "[7 - 8.5]")
+  
+ pal <- brewer.pal(6, "Spectral")
+ 
+#carte de la médiane avec tmap en faisant des breaks
+
+ df_med <- as.data.frame(rast_median_cent, xy = TRUE, na.rm = TRUE)
+ names(df_med)[3] <- "pH"
+ 
+ df_med$pH_cls <- cut(df_med$pH,
+                      breaks = brk,
+                      labels = labs,
+                      right  = FALSE)      
+ 
+
+ ggplot(df_med) +
+   geom_raster(aes(x, y, fill = pH_cls)) +
+   scale_fill_manual(values = pal,
+                     drop   = FALSE,       
+                     name   = "pH") +
+   coord_equal() +
+   labs(title = "Prédiction de la valeur médiane du pH") +
+   theme_minimal() +
+   theme(
+     legend.position = "right",
+     legend.title    = element_text(face = "bold"),
+     legend.text     = element_text(size = 8),
+     plot.title      = element_text(face = "bold", hjust = 0.5)
+   )
+########################################
+ 
+ df_cent <- as.data.frame(rast_median_cent, xy = TRUE, na.rm = TRUE) |>
+   rename(pH = 3) |>
+   mutate(pH_cls = cut(pH, breaks = brk, labels = labs, right = FALSE),
+          raster  = "Modèle_centroïdess")
+ 
+ 
+ df_median  <- as.data.frame(rast_median, xy = TRUE, na.rm = TRUE) |>
+   rename(pH = 3) |>
+   mutate(pH_cls = cut(pH, breaks = brk, labels = labs, right = FALSE),
+          raster  = "Modèle_données ponctuelles ") 
+ 
+ df_all  <- bind_rows(df_cent, df_median) 
+
+ ggplot(df_all) +
+   geom_raster(aes(x, y, fill = pH_cls)) +
+   facet_wrap(~ raster, ncol = 2) +          
+   scale_fill_manual(values = pal,
+                     drop   = FALSE,
+                     name   = "pH") +
+   coord_equal() +
+   labs(title = "Prédictions médianes du pH – comparaison de 2 modèles") +
+   theme_minimal() +
+   theme(
+     legend.position = "right",
+     legend.title    = element_text(face = "bold"),
+     legend.text     = element_text(size = 8),
+     strip.text      = element_text(face = "bold"),
+     plot.title      = element_text(face = "bold", hjust = 0.5)
+   )
+ 
--- Randon_forest_1.R
+++ Randon_forest_1.R
@@ -1,8 +1,6 @@
 #Chargement des packages nécessaires ----
-library(sf)          # manipulation de données spatiales
 library(dplyr)       # manipulation de données
 library(Boruta)      # sélection des variables importantes
-library(randomForest) 
 library(ranger)      # random forest rapide
 library(ggplot2)     # visualisation
 library(terra)       # manipulation raster
@@ -15,51 +13,13 @@ library(tidyr)       # fonctions tidyverse
 
 datacov <- readRDS("Y:/BDAT/traitement_donnees/MameGadiaga/resultats/matrice_covariables.rds")
 
-#Définition des fonctions ----
-Myeval <- function(x, y){
-  
-  # mean error
-  ME <- round(mean(y - x, na.rm = TRUE), digits = 2)
-  
-  # root mean square error
-  RMSE <-   round(sqrt(mean((y - x)^2, na.rm = TRUE)), digits = 2)
-  
-  # root mean absolute error
-  MAE <-   round(mean(abs(y - x), na.rm = TRUE), digits = 2)
-  
-  # Pearson's correlation squared
-  r2 <-  round((cor(x, y, method = 'pearson', use = 'pairwise.complete.obs')^2), digits = 2)
-  
-  # MEC
-  SSE <- sum((y - x) ^ 2, na.rm = T)
-  SST <- sum((y - mean(y, na.rm = T)) ^ 2, na.rm = T)
-  NSE <- round((1 - SSE/SST), digits = 2)
-  
-  # concordance correlation coefficient
-  n <- length(x)
-  sdx <- sd(x, na.rm = T)
-  sdy <- sd(y, na.rm = T)
-  r <- stats::cor(x, y, method = 'pearson', use = 'pairwise.complete.obs')
-  
-  # return the results
-  evalRes <- data.frame(ME = ME, MAE = MAE, RMSE = RMSE, r = r, r2 = r2, NSE = NSE)
-  
-  return(evalRes)
-}
-
-#Définition des variables ------
-
-kmax= 5 # pour la parallelisation, le nb de coeurs
-ntree = 400 # le nbre d'arbre de random forest
-k=10 # pour la k fold cross validation
-
 
 #Nettoyage des données ----
 datacov<-datacov %>% 
   select(-c(id_profil,bdatid,insee,codification)) %>% 
   na.omit()
 
-covs <- datacov[, 7:70]
+covs <- datacov[, 6:70]
 summary(covs)
 
 summary(datacov)
@@ -67,8 +27,6 @@ summary(datacov)
 # Échantillonnage à 5000 points pour réduire la taille du jeu de données
 set.seed(123)
 datacov_s <- datacov %>% sample_n(5000)
-datacov_s <- datacov_s %>% 
-  st_drop_geometry()
 
 # Ajout d’un identifiant unique pour la validation croisée
 datacov_s$id <- 1:nrow(datacov_s)
@@ -85,6 +43,7 @@ Y <- datacov_s[[idvar]]
 
 #Calibration automatique de mtry pour Random Forest ----
 
+ntree <- 400  # nombre d’arbres à utiliser
 taille <- length(idcovs)  # nombre de covariables disponibles
 
 # Calibration du paramètre mtry avec tuneRF
@@ -156,54 +115,3 @@ ggplot(Imp_QRF, aes(x = reorder(Variable, Importance), y = Importance)) +
   ggtitle("Importance des covariables sélectionnées par Boruta")
 
 # Validation croisée à 10 plis ----
-
-cat(" Validation croisée 10 plis ----------------\n")
-
-# Définir le nombre de plis
-k <- k
-set.seed(789)
-folds <- cut(seq(1, nrow(datacov_shrt)), breaks = k, labels = FALSE)
-
-# Initialisation
-datacov_shrt$predRF <- NA
-
-resuXval <- foreach(i = 1:k, .errorhandling = 'pass') %do% {
-  cat("Fold", i, "\n")
-  
-  # Séparer les données
-  test_indices <- which(folds == i, arr.ind = TRUE)
-  test_data <- datacov_shrt[test_indices, ]
-  train_data <- datacov_shrt[-test_indices, ]
-  
-  # Entraîner le modèle
-  QRF_Mod.G <- ranger(
-    formula = formule.ranger,
-    data = train_data[, !(names(train_data) %in% c("id", "predRF"))],
-    num.trees = ntree,
-    min.node.size = 3,
-    quantreg = TRUE,
-    max.depth = 15,
-    mtry = mtry_opt,
-    importance = "permutation",
-    scale.permutation.importance = FALSE,
-    keep.inbag = FALSE
-  )
-  
-  # Prédire sur l'ensemble de test
-  preds <- predict(QRF_Mod.G,
-                   data = test_data[, cov_brt[-1]],  # retirer pH
-                   type = "quantiles",
-                   quantiles = 0.5,
-                   num.threads = parallel::detectCores())$predictions
-  
-  # Stocker les prédictions
-  datacov_shrt$predRF[test_indices] <- round(preds, 2)
-  
-  # Retourner quelque chose pour chaque pli si nécessaire
-  list(fold = i, indices = test_indices, predictions = preds)
-}
-
-# Calculer les statistiques de validation croisée
-resuXvalQRF <-  Myeval(datacov_shrt$predRF,   datacov_shrt$pH )
-
-resuXvalQRF
